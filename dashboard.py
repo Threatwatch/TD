5a6a477cd6e32ca549df5f0e33ef8cb66b0ade30
Faisal <143286292+FSBARGAN@users.noreply.github.com>
2024-09-24 16:55:51 +0300

    remove old url

--- dashboard.py
+++ dashboard.py
@@ -37,7 +37,6 @@ channel_urls = [
     "https://t.me/Team_R70",
     "https://t.me/Arab_Hackers_Union",
     "https://t.me/AnonymousEgypt",
-    "https://t.me/+0EHsVXVuasEyOGY0",
     "https://t.me/CyberS102"
 ]
 
